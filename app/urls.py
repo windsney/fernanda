dcb7c5fd6f36e1368acd42643ca59b0407cc09a5
windsney <windsney@hotmail.com>
2026-03-31 20:30:58 -0300

    final

---
+++ app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import Index
+
+
+urlpatterns = [
+    path('', Index.as_view(), name='index'),
+   
+# CBV básica
+    #path('minhaview/', MinhaView.as_view(), name='minha-view'),
+    
+    # TemplateView
+    
+    
+    # ListView
+   # path('itens/', ListaItensView.as_view(), name='lista-itens'),
+    
+    # DetailView com parâmetro na URL
+   # path('item/<int:pk>/', DetalheItemView.as_view(), name='detalhe-item'),
+
+]
---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView,ListView,DetailView
+
+
+class Index(TemplateView):
+    template_name = 'app/index.html'
+
+'''
+class ListaItensView(ListView):
+    model = MinhaModel
+    template_name = 'meuapp/lista.html'
+    context_object_name = 'itens'
+
+class DetalheItemView(DetailView):
+    model = MinhaModel
+    template_name = 'app/detalhe.html'
+    context_object_name = 'item'
+
+'''
